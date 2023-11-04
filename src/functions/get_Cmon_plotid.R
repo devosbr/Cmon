a160567273053606f91e84a6ceac9b6a097538a4
devosbr <bruno.devos@inbo.be>
2023-11-04 10:46:17 +0100

    Rewriting function for identification Cmon PLOTID

--- src/functions/get_Cmon_plotid.R
+++ src/functions/get_Cmon_plotid.R
@@ -1,42 +1,35 @@
-#############################
+###########################--
 ## Get Cmon plotid
-## Bruno De Vos 14/05/2019
-#############################
+## Bruno De Vos 04/11/2023
+##########################--
 
+# Init ----
 library(raster)
-
-##############--------------------------------------------------
-#Load raster for reference =>> make raster with plotnrs instead of legend ? 
-
-r.LUCMon<-raster("C:/R_scripts/R_Raster/DATA/LUCMon2016trim.tif")  ### 215513424 cells in trimmed version
-leg<-c("1.Forest","2.Nature","3.Grassland","4.Cropland",
-       "5.Residential","6.Sealed","7.Water","8.Unclassified")
-mycol<-c("dark green","yellow","light green","orange",
-         "magenta","red","blue","light grey")
-veldnamen<-c("PlotNo","PlotID","c.LB72X","c.LB72Y","LUCMON","GRTS.Rank")
-
-plot(r.LUCMon)
-
-#### provide Lambert72 coord list
-LB72X<-c(137525)
-LB72Y<-c(195925)
+setwd("C:/R_scripts/_GIT_REPO/Cmon/data/")
 
 
+# 1. Load 10x10m raster for georeference ----  
+# n= 215513424 grid cells
+# extent LB72 metric: 22000, 258880, 153050, 244030  (xmin, xmax, ymin, ymax)
+# values are 2016 landuse c("1.Forest","2.Nature","3.Grassland","4.Cropland",
+# "5.Residential","6.Sealed","7.Water","8.Unclassified")
+r.LUCMon<-raster("./Cmon_base_raster_10x10.tif")  ###  in trimmed version
+# optional: plot(r.LUCMon)
 
-#### function givePLOTID
+# 2. generic function getplotID with LB72 XY coords as input ----
 getplotid<-function(LB72X,LB72Y) {
 coordlist<-cbind(LB72X,LB72Y)  
 ### Determine cell in raster for x,y
-PLOTNRS<-cellFromXY(r.LUCMon,coordlist)
+PLOTNR<-cellFromXY(r.LUCMon,coordlist)
 ### Determine PLOTIDs for these cells
-PLOTID<-paste0("P",as.character(as.hexmode(PLOTNRS)))
+PLOTID<-paste0("P",as.character(as.hexmode(PLOTNR)))
 return(PLOTID)
 }
 
 
 ## checking
-getplotid(137525,195925)
-points(137525,195925,cex=1,pch=16)
-getplotid(69085,187275)  #=> OK
-getplotid(185735,229175) #=> OK
+#getplotid(128114.99,156835)
+#points(137525,195925,cex=1,pch=16)
+
+
 
